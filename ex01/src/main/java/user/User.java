c42419e57d269556bfd6d0bbf9c22982d46b353d
BonggilB <95198463+BonggilB@users.noreply.github.com>
2022-03-22 18:10:51 +0900

    Add files via upload

---
+++ ex01/src/main/java/user/User.java
@@ -0,0 +1,43 @@
+package user;
+
+public class User {
+	private String uID;
+	private String uPass;
+	private String uName;	
+	private String uAddress;
+	private String uEmail;
+	
+	
+	public String getuID() {
+		return uID;
+	}
+	public void setuID(String uID) {
+		this.uID = uID;
+	}
+	public String getuPass() {
+		return uPass;
+	}
+	public void setuPass(String uPass) {
+		this.uPass = uPass;
+	}
+	public String getuName() {
+		return uName;
+	}
+	public void setuName(String uName) {
+		this.uName = uName;
+	}
+	public String getuAddress() {
+		return uAddress;
+	}
+	public void setuAddress(String uAddress) {
+		this.uAddress = uAddress;
+	}
+	public String getuEmail() {
+		return uEmail;
+	}
+	public void setuEmail(String uEmail) {
+		this.uEmail = uEmail;
+	}
+	
+	
+}
---
+++ ex01/src/main/java/user/UserDAO.java
@@ -0,0 +1,75 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/bbs2";			
+			String dbID = "root";
+			String dbPassword = "root";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int login(String uID, String uPass) {
+		String SQL = "SELECT uPass FROM USER WHERE uID = ?";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1,  uID);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				if (rs.getString(1).equals(uPass)) {
+					return 100;
+				} else {
+					return -100;
+				}
+			}
+			return -200;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -300; 
+	}
+	
+	
+	public int join(User user) {
+		String SQL = "insert into user values(?,?,?,?,?)";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1,user.getuID());
+			pstmt.setString(2,user.getuPass());
+			pstmt.setString(3,user.getuName());
+			pstmt.setString(4,user.getuAddress());
+			pstmt.setString(5,user.getuEmail());
+			return pstmt.executeUpdate();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -300;
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
